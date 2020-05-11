540c6275214c1ed8a7cb1965d50dd0b983867b30
Jason Franklin <jfrank325@gmail.com>
2020-05-11 13:44:52 +0200

    env changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import './assets/main.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
 import axios from 'axios';
 
 axios.get('/api/auth/loggedin').then((response) => {
