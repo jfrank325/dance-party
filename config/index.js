fc106af48128d700c1d8bde5ed66e5feb44ea0bd
Jason Franklin <jfrank325@gmail.com>
2020-05-20 18:09:22 +0200

    variable made for mlab

---
+++ config/index.js
@@ -0,0 +1,3 @@
+const MONGODB_URI = `mongodb://dancertalk:Dancertalk523@ds011810.mlab.com:11810/heroku_frkxz6hx`;
+
+module.exports = MONGODB_URI;
--- db/index.js
+++ db/index.js
@@ -1,21 +1,21 @@
-const mongoose = require("mongoose");
-
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useFindAndModify", false);
-mongoose.set("useCreateIndex", true);
-mongoose.set("useUnifiedTopology", true);
+const mongoose = require('mongoose');
+const URI = require('../config/index');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
 mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(conn => {
+  .connect(process.env.MONGODB_URI || URI)
+  .then((conn) => {
     console.log(`Connected to ${conn.connections[0].name}`);
   })
-  .catch(err => {
+  .catch((err) => {
     console.log(`Error connecting to the DB: ${err}`);
   });
 
 module.exports = {
   disconnect: () => {
     mongoose.connection.close();
-  }
+  },
 };
