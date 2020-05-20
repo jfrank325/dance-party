3c9a2c4821c9bce72c9e8c50e1b7549b5ae31e12
Jason Franklin <jfrank325@gmail.com>
2020-05-20 18:13:06 +0200

    variable made for mlab

--- client/src/index.js
+++ client/src/index.js
@@ -6,14 +6,15 @@ import './assets/main.css';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios';
 
-axios.get('/api/auth/loggedin').then((response) => {
-  ReactDOM.render(
-    <BrowserRouter>
-      <App user={response.data} />
-    </BrowserRouter>,
-    document.getElementById('root')
-  );
-});
+// axios.get('/api/auth/loggedin').then((response) => {
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+// from App   user={response.data}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
