a1e1d1e1e9ceda1bcc9830d5db5825bad3d80d58
Jason Franklin <jfrank325@gmail.com>
2020-05-21 11:41:31 +0200

    gitignore changed dirname back

---
+++ client/build/precache-manifest.2c4afcd5ec2126e57f291ca43649be5f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37465081166268d803ca8658b1a911b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07cdc75a1560ec3d657a",
+    "url": "/static/css/main.7d9c0223.chunk.css"
+  },
+  {
+    "revision": "3b237b0a31475c5f7cc0",
+    "url": "/static/js/2.17861a4d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.17861a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07cdc75a1560ec3d657a",
+    "url": "/static/js/main.c422bba8.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  },
+  {
+    "revision": "502429c08513749dabd0f19fd01b1f85",
+    "url": "/static/media/Bin.502429c0.png"
+  },
+  {
+    "revision": "23119b442abf504501883580de4c9c71",
+    "url": "/static/media/DownArrow.23119b44.jpg"
+  },
+  {
+    "revision": "1ea3a40bfbb56a7add225c3115d34d9c",
+    "url": "/static/media/Pencil1.1ea3a40b.png"
+  },
+  {
+    "revision": "c7a25ef46a16b48c58a12525239b8ab2",
+    "url": "/static/media/UpArrow.c7a25ef4.jpg"
+  }
+]);
