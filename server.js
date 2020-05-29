0ca00474a83d1faeb199a8caf16972138c6c24d4
Jason Franklin <jfrank325@gmail.com>
2020-05-29 14:38:09 +0200

    http to https added

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const app = express();
 
 require('./session')(app);
 
+const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
+
+// Don't redirect if the hostname is `localhost:port` or the route is `/insecure`
+app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/], 301));
 // Allow cross-origin
+
 app.use(cors());
 
 app.use(express.urlencoded({ extended: true })); // sets the `body` object in the `request` with the values from an HTML POST form
