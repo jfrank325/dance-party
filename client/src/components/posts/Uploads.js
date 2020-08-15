84a214ed72764b301267bbf4bf60ae7621bfd0b7
Jason Franklin <jfrank325@gmail.com>
2020-08-15 20:12:57 +0200

    upload styling fixed

--- client/src/components/posts/Uploads.js
+++ client/src/components/posts/Uploads.js
@@ -3,10 +3,22 @@ import React from 'react';
 const Uploads = ({ uploadImage, uploadVideo, loading }) => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <label htmlFor="imgPath">Upload Image</label>
-      <input id="upload" type="file" name="imgPath" onChange={uploadImage} />
-      <label htmlFor="videoPath">Upload Video</label>
-      <input id="upload" style={{ marginBottom: '1.9rem' }} type="file" name="videoPath" onChange={uploadVideo} />
+      <div style={{ display: 'flex' }}>
+        <label className="upload" htmlFor="imgPath">
+          <input id="imgPath" type="file" name="imgPath" onChange={uploadImage} />
+          Upload Image
+        </label>
+        <label className="upload" htmlFor="videoPath">
+          <input
+            id="videoPath"
+            style={{ marginBottom: '1.9rem' }}
+            type="file"
+            name="videoPath"
+            onChange={uploadVideo}
+          />
+          Upload Video
+        </label>
+      </div>
       {loading === 'loading' ? <div className="donut"></div> : loading === 'finished' ? <h4>Finished</h4> : <> </>}
     </div>
   );
