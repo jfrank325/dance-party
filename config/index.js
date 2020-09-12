d772fc85e054b825f38913f19a7a171b873b541e
Jason Franklin <jfrank325@gmail.com>
2020-09-12 19:24:29 +0200

    atlas settings

--- config/index.js
+++
@@ -1,3 +0,0 @@
-const MONGODB_URI = `mongodb://dancertalk:Dancertalk523@ds011810.mlab.com:11810/heroku_frkxz6hx`;
-
-module.exports = MONGODB_URI;
--- db/index.js
+++ db/index.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
-const URI = require('../config/index');
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
 mongoose
-  .connect(process.env.MONGODB_URI || URI)
+  .connect(process.env.MONGODB_URI)
   .then((conn) => {
     console.log(`Connected to ${conn.connections[0].name}`);
   })
